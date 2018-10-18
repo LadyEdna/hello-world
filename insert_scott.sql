78139bc8a92355c63dc0d4b6cb92c3c95bb09362
LadyEdna <44264310+LadyEdna@users.noreply.github.com>
2018-10-18 15:03:03 +0200

    Add files via upload

---
+++ insert_scott.sql
@@ -0,0 +1,32 @@
+--Daten in die Tabelle EMP einfügen
+Insert into EMP (EMPNO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DEPTNO) values ('7369','SMITH','CLERK','7902','17.12.80','800',null,'20');
+Insert into EMP (EMPNO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DEPTNO) values ('7499','ALLEN','SALESMAN','7698','20.02.81','1600','300','30');
+Insert into EMP (EMPNO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DEPTNO) values ('7521','WARD','SALESMAN','7698','22.02.81','1250','500','30');
+Insert into EMP (EMPNO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DEPTNO) values ('7566','JONES','MANAGER','7839','02.04.81','2975',null,'20');
+Insert into EMP (EMPNO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DEPTNO) values ('7654','MARTIN','SALESMAN','7698','28.09.81','1250','1400','30');
+Insert into EMP (EMPNO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DEPTNO) values ('7698','BLAKE','MANAGER','7839','01.05.81','2850',null,'30');
+Insert into EMP (EMPNO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DEPTNO) values ('7782','CLARK','MANAGER','7839','09.06.81','2450',null,'10');
+Insert into EMP (EMPNO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DEPTNO) values ('7788','SCOTT','ANALYST','7566','19.04.87','3000',null,'20');
+Insert into EMP (EMPNO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DEPTNO) values ('7839','KING','PRESIDENT',null,'17.11.81','5000',null,'10');
+Insert into EMP (EMPNO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DEPTNO) values ('7844','TURNER','SALESMAN','7698','08.09.81','1500','0','30');
+Insert into EMP (EMPNO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DEPTNO) values ('7876','ADAMS','CLERK','7788','23.05.87','1100',null,'20');
+Insert into EMP (EMPNO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DEPTNO) values ('7900','JAMES','CLERK','7698','03.12.81','950',null,'30');
+Insert into EMP (EMPNO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DEPTNO) values ('7902','FORD','ANALYST','7566','03.12.81','3000',null,'20');
+Insert into EMP (EMPNO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DEPTNO) values ('7934','MILLER','CLERK','7782','23.01.82','1300',null,'10');
+
+--Daten in die Tabelle SALGRADE einfügen
+Insert into SALGRADE (GRADE,LOSAL,HISAL) values ('1','700','1200');
+Insert into SALGRADE (GRADE,LOSAL,HISAL) values ('2','1201','1400');
+Insert into SALGRADE (GRADE,LOSAL,HISAL) values ('3','1401','2000');
+Insert into SALGRADE (GRADE,LOSAL,HISAL) values ('4','2001','3000');
+Insert into SALGRADE (GRADE,LOSAL,HISAL) values ('5','3001','9999');
+
+--Daten in die Tabelle DEPT einfügen
+Insert into DEPT (DEPTNO,DNAME,LOC) values ('10','ACCOUNTING','NEW YORK');
+Insert into DEPT (DEPTNO,DNAME,LOC) values ('20','RESEARCH','DALLAS');
+Insert into DEPT (DEPTNO,DNAME,LOC) values ('30','SALES','CHICAGO');
+Insert into DEPT (DEPTNO,DNAME,LOC) values ('40','OPERATIONS','BOSTON');
+
+select * from emp;
+select * from salgrade;
+select * from dept;
